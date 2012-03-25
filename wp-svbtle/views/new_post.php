6d0ba309744250cc336b8cabb5423b58b1eff736
Ricardo Rauch <ricardo@gravityonmars.com>
2012-03-25 20:22:59 -0300

    fixes

--- wp-svbtle/views/new_post.php
+++ wp-svbtle/views/new_post.php
@@ -6,58 +6,51 @@ Template Name: New Post
 require_once WPSUBTLE_PATH . 'views/process_post.php'; 
 
 nocache_headers();
+include('header.php');
 ?>
-		<div id="container">
-			<div id="content" role="main">
-				<div id="formbox">
-				<?php
-					if ($err != "") {
-						echo "<p>".$err."</p>";
-					}
-				?>
-				<?php
-					if ($_GET['success'] == "success") {
-						echo "<p>Your post was successfully submitted.</p>";
-					}
-					else { ?>
-						<?php if (is_user_logged_in()) { // checking weather or not the user has logged in.
-						?>
-						<form action="" method="post" enctype="multipart/form-data">
-							<input type="hidden" name="action" value="post" />
-							<?php wp_nonce_field( 'new-post' ); ?>
-								<p>
-									<input type="text" id="post_title" name="post_title" value="<?php echo $post_title;?>" size="60" tabindex="1"/>
-									<label for="title">Post Title</label>
-								</p>
+<div class="wrap">
+	<?php
+		if ($err != "") {
+			echo "<p>".$err."</p>";
+		}
+	?>
+	<?php
+		if ($_GET['success'] == "success") {
+			echo "<p>Your post was successfully submitted.</p>";
+		}
+		else { ?>
+			<?php if (is_user_logged_in()) { // checking weather or not the user has logged in.
+			?>
+			<form action="" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="post" />
+				<?php wp_nonce_field( 'new-post' ); ?>
+				<p>
+					<input type="text" id="post_title" name="post_title" value="<?php echo $post_title;?>" placeholder="Title Here" size="60" tabindex="1"/>
+				</p>
 
-								<p>
-									<?php wp_dropdown_categories('show_option_none=Choose Post Category&orderby=name&order=ASC&hide_empty=0&hierarchical=1'); ?>
-									<label for="cat">Post Category</label>
-								</p>	
+				<p>
+					<textarea name="post_content" id="post_content" cols=88 rows=20 tabindex="2"><?php echo $post_content; ?></textarea>
+				</p>
 
-								<p><label for="post_content">Post Content:</label><br />
-									<script type="text/javascript" src="<?php echo get_template_directory_uri(). '/tiny_mce/'; ?>tiny_mce.js"></script>
-									<textarea name="post_content" id="post_content" cols=88 rows=20 tabindex="2"><?php echo $post_content; ?></textarea>
-										<script type="text/javascript">
-										tinyMCE.init({
-											theme : "advanced",
-											mode : "textareas",
-										});
-										</script>
-								</p>
+				<input id="submit" type="submit" value="Submit Post" />
 
-								<p>
-									<input type="text" id="post_tags" name="post_tags" value="<?php echo $post_tags;?>" size="60" tabindex="3"/>
-									<label for="post_tags">Post Tags</label>
-								</p>
-								<input id="submit" type="submit" value="Submit Post" />
+			</form>
+			<?php } else { ?>
+			<p>Sorry, you don't have permission to post new article!</p>
+			<?php } ?>
+		<?php } ?>
+		
+</div><!-- .wrap -->
 
-						</form>
-						<?php } else { ?>
-						<p>Sorry, you don't have permission to post new article!</p>
-						<?php } ?>
-					<?php } ?>
-				</div>
-			</div><!-- #content -->
-		</div><!-- #container -->
+<div class="buttons">
+	<a href="#" class="button">Preview</a>
+	<a href="#" class="button">Option</a>
+	
+	<div class="double">
+		<a href="#" class="button checked">Idea</a>
+		<a href="#" class="button checked">Public</a>
+	</div>
+	
+	<a href="#" class="button">Save</a>
 
+</div><!-- .buttons -->
