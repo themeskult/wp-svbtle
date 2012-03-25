a50c14074113834eda72cd99ed8139e2b60bbcb3
Ricardo Rauch <ricardo@gravityonmars.com>
2012-03-25 20:33:07 -0300

    fixes

--- wp-svbtle/views/new_post.php
+++ wp-svbtle/views/new_post.php
@@ -8,6 +8,8 @@ require_once WPSUBTLE_PATH . 'views/process_post.php';
 nocache_headers();
 include('header.php');
 ?>
+<form action="" method="post" enctype="multipart/form-data">
+
 <div class="wrap">
 	<?php
 		if ($err != "") {
@@ -21,20 +23,17 @@ include('header.php');
 		else { ?>
 			<?php if (is_user_logged_in()) { // checking weather or not the user has logged in.
 			?>
-			<form action="" method="post" enctype="multipart/form-data">
 				<input type="hidden" name="action" value="post" />
 				<?php wp_nonce_field( 'new-post' ); ?>
 				<p>
-					<input type="text" id="post_title" name="post_title" value="<?php echo $post_title;?>" placeholder="Title Here" size="60" tabindex="1"/>
+					<input type="text" id="post_title" class="text" name="post_title" value="<?php echo $post_title;?>" placeholder="Title Here" size="60" tabindex="1"/>
 				</p>
 
 				<p>
 					<textarea name="post_content" id="post_content" cols=88 rows=20 tabindex="2"><?php echo $post_content; ?></textarea>
 				</p>
 
-				<input id="submit" type="submit" value="Submit Post" />
 
-			</form>
 			<?php } else { ?>
 			<p>Sorry, you don't have permission to post new article!</p>
 			<?php } ?>
@@ -47,10 +46,11 @@ include('header.php');
 	<a href="#" class="button">Option</a>
 	
 	<div class="double">
-		<a href="#" class="button checked">Idea</a>
-		<a href="#" class="button checked">Public</a>
+		<a href="#" class="button checked">&#10004;	Idea</a>
+		<a href="#" class="button ">Public</a>
 	</div>
 	
 	<a href="#" class="button">Save</a>
 
 </div><!-- .buttons -->
+</form>
