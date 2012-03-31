661734d8dccb63cbb4106105759ca2568ad07178
Ricardo Rauch <ricardo@gravityonmars.com>
2012-03-31 14:07:26 -0300

    fixes

--- wp-content/themes/svbtle/loop.php
+++ wp-content/themes/svbtle/loop.php
@@ -1,9 +1,6 @@
 <?php /* Display navigation to next/previous pages when applicable */ ?>
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
-	<nav id="nav-above" class="navigation">
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'boilerplate' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'boilerplate' ) ); ?></div>
-	</nav><!-- #nav-above -->
+
 <?php endif; ?>
 
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
