1cba96b7b1f7c69cc6194fea70d9d52ba6989e59
Andrew Freeman <andrew.s.freeman@gmail.com>
2012-06-05 19:55:44 -0700

    Cleaner commit for issue #9. Doesn`t have all my my print_rs everywhere, confusing the generated diff file. :)

--- wp-content/themes/svbtle/theme-options.php
+++ wp-content/themes/svbtle/theme-options.php
@@ -4,7 +4,7 @@ add_action( 'admin_init', 'theme_options_init' );
 add_action( 'admin_menu', 'theme_options_add_page' );
 
 function theme_options_init(){
-	register_setting( 'sample_options', 'svbtle_options', 'theme_options_validate' );
+	register_setting( 'sample_options', 'svbtle_options' );
 }
 
 function theme_options_add_page() {
@@ -33,15 +33,21 @@ function theme_options_do_page() {
 
 		<form method="post" action="options.php">
 			<?php settings_fields( 'sample_options' ); ?>
-			<?php $options = get_option( 'svbtle_options' ); ?>
+			<?php $options = get_option( 'svbtle_options' ); 
+				  if( ! is_null( $options['color'] ) && '' != $options['color'] )
+				  	$color = esc_attr( $options['color'] );
+				  else
+				  	$color = '#ff0000';
+			?>
 
 				<h3><?php _e( 'Twitter Username', 'wordpress-svbtle' ); ?></h3>
-				<input class="regular-text" type="text" name="svbtle_options[twitter_username]" value="<?php esc_attr_e( $options['twitter_username'] ); ?>" /><div id="color_picker_color1"></div>
+				<input class="regular-text" type="text" name="svbtle_options[twitter_username]" value="<?php esc_attr_e( $options['twitter_username'] ); ?>" />
 				
 				
 				<h3><?php _e( 'Blog color', 'wordpress-svbtle' ); ?></h3>
 				<p>
-						<input id="color1" class="regular-text" type="text" name="svbtle_options[color]" value="<?php esc_attr_e( $options['color'] ); ?>" /><div id="color_picker_color1"></div>
+					<div id="color_picker_color1"></div>
+					<input id="color1" class="regular-text" type="text" name="svbtle_options[color]" value="<?php echo $color; ?>" />
 				</p>
 				<h3><?php _e( 'Google Analytics // Typekit', 'wordpress-svbtle' ); ?></h3>
 				<p>
@@ -70,8 +76,8 @@ function theme_options_validate( $input ) {
 
 	$input['color'] = wp_filter_nohtml_kses( $input['color'] );
 
-	if ( ! array_key_exists( $input['selectinput'], $select_options ) )
-		$input['selectinput'] = null;
+	/*if ( ! @array_key_exists( $input['selectinput'], $select_options ) )
+		$input['selectinput'] = null; */
 
 	$input['google_analytics'] = $input['google_analytics'] ;
 
