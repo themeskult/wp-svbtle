c7a7cabebacded2271d0686d516b18384b6d5053
Scott Rocher <scottrocher@gmail.com>
2012-06-17 10:56:48 -0300

    Add a clean post title to <title> tag when the page is an individual post.

--- wp-content/themes/svbtle/header.php
+++ wp-content/themes/svbtle/header.php
@@ -6,7 +6,7 @@
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<title><?php bloginfo( 'name' ); ?></title>
+		<title><?php if ( is_singular() ) { echo trim(wp_title("")) . ' | '; } ?><?php bloginfo( 'name' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
