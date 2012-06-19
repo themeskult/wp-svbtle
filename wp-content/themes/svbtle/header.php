81cf3f3f54941327e7941e8f30e5e6624f09a47a
Ricardo Rauch <ricardo@gravityonmars.com>
2012-06-19 19:00:19 -0300

    responsive

--- wp-content/themes/svbtle/header.php
+++ wp-content/themes/svbtle/header.php
@@ -53,7 +53,7 @@ if( isset( $options['color'] ) && '' != $options['color'] ) {
 		
 			
 			<h1><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<p><?php bloginfo( 'description' ); ?></p>
+			<h2><?php bloginfo( 'description' ); ?></h2>
 			
 			<?php 
 			ob_start(); 
