2358200ee11cefc4e75e2b1494ac6a5797c3153e
Abbie Gonzalez <abbie@abbiecod.es>
2012-06-20 14:31:19 -0400

    fix look in iOS6
    responsive look works in iOS6

--- wp-content/themes/svbtle/header.php
+++ wp-content/themes/svbtle/header.php
@@ -6,6 +6,7 @@
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width" />
 		<title><?php bloginfo( 'name' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
