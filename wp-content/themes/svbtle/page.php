ba62b3849b449051acb273ba3ffc54975d8a2c8e
Ricardo Rauch <ricardo@gravityonmars.com>
2012-07-21 18:55:35 -0300

    more new fixes

--- wp-content/themes/svbtle/page.php
+++ wp-content/themes/svbtle/page.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
 
-get_header(); 
+get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-get_template_part( 'loop', 'index' );
+<article id="<?php the_ID(); ?>" class="post">
+	<h2>
+		<?php print_post_title() ?>
+	</h2>
+	<?php the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'hbd-theme' )  ); ?>
+	
+	<h2><?php //echo get_the_category_list(', '); ?></h2>
+</article>
+<?php endwhile; // End the loop. Whew. ?>
 
-get_footer(); ?>
+
+<?php get_footer(); ?>
