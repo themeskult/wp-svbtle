4504f1d43d02326f8c3fbe14ad639e280b39e86c
Ricardo Rauch <ricardo@gravityonmars.com>
2012-07-22 15:46:27 -0300

    fixes comments

--- wp-content/themes/svbtle/single.php
+++ wp-content/themes/svbtle/single.php
@@ -24,7 +24,7 @@ get_template_part( 'loop', 'index' );
 
 <nav class="pagination">
   <span class="prev">
-    <a href="http://dcurt.is" class="back_to_blog">←&nbsp;&nbsp;&nbsp;Back to blog</a>
+    <a href="<?php echo home_url( '/' ); ?>" class="back_to_blog">←&nbsp;&nbsp;&nbsp;Back to blog</a>
   </span>
 </nav>
 <?php get_footer(); ?>
