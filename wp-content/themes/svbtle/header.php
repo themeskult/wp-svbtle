d766a3fca3a639bf139d81407006dbd6d4a4af96
Ricardo Rauch <ricardo@gravityonmars.com>
2012-07-22 16:31:16 -0300

    wordpress.com sharing

--- wp-content/themes/svbtle/header.php
+++ wp-content/themes/svbtle/header.php
@@ -9,7 +9,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5, minimum-scale=0.5">
 		<title><?php bloginfo( 'name' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
 
