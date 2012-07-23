921ffbd1bfafdb578c6b170883bf7916d37639fd
Ricardo Rauch <ricardo@gravityonmars.com>
2012-07-23 17:58:04 -0300

    support for external urls

--- wp-content/themes/svbtle/functions.php
+++ wp-content/themes/svbtle/functions.php
@@ -377,7 +377,7 @@ $link = $perm;
 }
 if ($is_link): ?>
 	<a href="<?php echo $link ?>" class="title"><?php echo the_title() ?></a>
-	<a href="<?php echo $link ?>" class="anchor"><img src="<?php echo get_bloginfo('stylesheet_directory') ?>/images/anchor.svg" class="scalable"></a>
+	<a href="<?php echo the_permalink() ?>" class="anchor"><img src="<?php echo get_bloginfo('stylesheet_directory') ?>/images/anchor.svg" class="scalable"></a>
 <?php else: ?>
 	<a href="<?php the_permalink(); ?>" class="no-link" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 
