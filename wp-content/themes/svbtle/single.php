e6d3a1ead1d9fbd9902589e53781c11c74278b64
Ricardo Rauch <ricardo@gravityonmars.com>
2012-09-14 15:07:23 -0300

    fixes

--- wp-content/themes/svbtle/single.php
+++ wp-content/themes/svbtle/single.php
@@ -24,7 +24,7 @@ get_template_part( 'loop', 'index' );
 
 <nav class="pagination">
   <span class="prev">
-    <a href="<?php echo home_url( '/' ); ?>" class="back_to_blog">←&nbsp;&nbsp;&nbsp;Back to blog</a>
+    <a href="<?php echo home_url( '/' ); ?>" class="back_to_blog">←&nbsp;&nbsp;&nbsp;read more</a>
   </span>
 </nav>
 <?php get_footer(); ?>
