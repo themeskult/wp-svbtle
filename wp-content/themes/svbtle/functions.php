39f103fa7a1210eaf8412bfe4fd4b7820131be7e
Ricardo Rauch <ricardo@gravityonmars.com>
2012-09-14 14:58:24 -0300

    fix issue #69

--- wp-content/themes/svbtle/functions.php
+++ wp-content/themes/svbtle/functions.php
@@ -456,6 +456,8 @@ function remove_kudos() {
 	$kudos = get_post_meta( $post_id , '_wp-svbtle-kudos', true );
 	$new_kudos = $kudos - 1;
 
+	if ($new_kudos < 0) $new_kudos = 0;
+
 	add_post_meta( $post_id, '_wp-svbtle-kudos', 1, true ) or update_post_meta( $post_id, '_wp-svbtle-kudos', $new_kudos );
 
 	header('HTTP/1.1 200 OK');
