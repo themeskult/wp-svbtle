51dfb260973096e77ee14e6d9b2a118931dd476d
ahmet alp balkan <ahmet@ahmetalpbalkan.com>
2012-09-20 12:28:09 -0700

    404 customizations

--- wp-content/themes/svbtle/404.php
+++ wp-content/themes/svbtle/404.php
@@ -7,14 +7,5 @@
  * @since Boilerplate 1.0
  */
 
-get_header(); ?>
-			<article id="post-0" class="post error404 not-found" role="main">
-				<h1><?php _e( 'Not Found', 'boilerplate' ); ?></h1>
-				<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'boilerplate' ); ?></p>
-				<?php get_search_form(); ?>
-				<script>
-					// focus on search field after it has loaded
-					document.getElementById('s') && document.getElementById('s').focus();
-				</script>
-			</article>
-<?php get_footer(); ?>
+wp_redirect(get_bloginfo('wpurl'),302);
+exit;
