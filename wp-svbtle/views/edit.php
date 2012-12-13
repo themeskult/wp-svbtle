a11db6cf73b229a8e4ae610f066edf20fd806c23
Ricardo Rauch <ricardo@gravityonmars.com>
2012-12-13 04:21:09 -0300

    removing #preview. It needs a button to show/hide when needed. #104. I will do it later.

--- wp-svbtle/views/edit.php
+++ wp-svbtle/views/edit.php
@@ -20,18 +20,18 @@ include('header.php');
 
 <div class="wrap">
 	
-	<div class="module"><!-- .post content -->
 
-<form action="" method="post" enctype="multipart/form-data">
-	<?php if ($err != ""): ?>
-		<?php echo "<p class='wps-notice'>".$err."</p>" ?>
-	<?php elseif (isset($_GET['edit']) and ($_GET['success'] == "success")): ?>
-		<?php echo "<p class='wps-notice'>Your post was successfully submitted.</p>" ?>
-	<?php elseif (isset($_GET['edit']) and ($_GET['edit'] == "success")): ?>
-		<?php echo "<p class='wps-notice'>Your post was successfully updated.</p>" ?>					
-	<?php endif ?>
+	<form action="" method="post" enctype="multipart/form-data">
+		<?php if ($err != ""): ?>
+			<?php echo "<p class='wps-notice'>".$err."</p>" ?>
+		<?php elseif (isset($_GET['edit']) and ($_GET['success'] == "success")): ?>
+			<?php echo "<p class='wps-notice'>Your post was successfully submitted.</p>" ?>
+		<?php elseif (isset($_GET['edit']) and ($_GET['edit'] == "success")): ?>
+			<?php echo "<p class='wps-notice'>Your post was successfully updated.</p>" ?>					
+		<?php endif ?>
 
-	<?php if (is_user_logged_in()): // checking weather or not the user has logged in.?>
+		<?php if (is_user_logged_in()): // checking weather or not the user has logged in.?>
+				
 			<?php if(isset($post_id)): ?>
 				<input type="hidden" name="action" value="edit" />
 				<input type="hidden" name="id" value="<?php echo $post_id; ?>" />
@@ -41,11 +41,9 @@ include('header.php');
 				<?php wp_nonce_field( 'new-post' ); ?>
 			<?php endif; ?>
 
-				<textarea  id="post_title" class="text expand" name="post_title" placeholder="Title Here" size="60" tabindex="1"><?php echo $post_title;?></textarea>
+			<textarea  id="post_title" class="text expand" name="post_title" placeholder="Title Here" size="60" tabindex="1"><?php echo $post_title;?></textarea>
 
-			<p>
-				<textarea name="post_content" id="post_content" placeholder="Write post here" class="content"  tabindex="2"><?php echo $post_content ?></textarea>
-			</p>
+			<textarea name="post_content" id="post_content" placeholder="Write post here" class="content"  tabindex="2"><?php echo $post_content ?></textarea>
 
 		<?php else: ?>
 			<?php // a lo mejor convendría un redirect? ?>
@@ -53,40 +51,34 @@ include('header.php');
 		<?php endif ?>
 
 		<div class="buttons">
-		<?php if (!empty($_GET['id'])): ?>
+			<?php if (!empty($_GET['id'])): ?>
 			<a href="<?php echo get_permalink($post_id) ?>" target="_blank" class="button preview">Preview</a>
-		<?php endif ?>
-		<a href="#external-url" class="open-external button">Option</a>
-		
-		<div class="double">
-			<input type="radio" class="RadioClass" name="post_status" value="draft" <?php if($post_status == 'draft' or empty($_GET['id'])): ?>checked="checked"<?php endif; ?> id="">
-			<a href="#" class="button <?php if(($post_status == 'draft') or empty($_GET['id'])): ?>checked<?php endif; ?>"><span class="tick">&#10004;</span>	Idea</a>
+			<?php endif ?>
+			<a href="#external-url" class="open-external button">Option</a>
 			
-			<input type="radio" class="RadioClass" name="post_status" value="publish" <?php if($post_status == 'publish'): ?>checked="checked"<?php endif; ?> id="">
-			<a href="#" class="button <?php if($post_status == 'publish'): ?>checked<?php endif; ?>"><span class="tick">&#10004;</span> Public</a>
-		</div>
-		<a href="index.php?page=edit&action=del&id=<?php echo $_GET['id'] ?>" class="button remove">Remove</a>
-		
-		
-		<div class="overlay">
-			<div id="external-url" >
-				<label>External Url</label>
-				<p><input type="text" placeholder="http://your-url.com" name="external_url" style="border: 1px solid black; padding: 4px; width: 300px" value="<?php echo $external_url ?>" id=""></p>
-				<input class="button close-fancy" type="button" value="OK" />
+			<div class="double">
+				<input type="radio" class="RadioClass" name="post_status" value="draft" <?php if($post_status == 'draft' or empty($_GET['id'])): ?>checked="checked"<?php endif; ?> id="">
+				<a href="#" class="button <?php if(($post_status == 'draft') or empty($_GET['id'])): ?>checked<?php endif; ?>"><span class="tick">&#10004;</span>	Idea</a>
+				
+				<input type="radio" class="RadioClass" name="post_status" value="publish" <?php if($post_status == 'publish'): ?>checked="checked"<?php endif; ?> id="">
+				<a href="#" class="button <?php if($post_status == 'publish'): ?>checked<?php endif; ?>"><span class="tick">&#10004;</span> Public</a>
 			</div>
-		</div><!-- .overlay -->
+			<a href="index.php?page=edit&action=del&id=<?php echo $_GET['id'] ?>" class="button remove">Remove</a>
+			
+			
+			<div class="overlay">
+				<div id="external-url" >
+					<label>External Url</label>
+					<p><input type="text" placeholder="http://your-url.com" name="external_url" style="border: 1px solid black; padding: 4px; width: 300px" value="<?php echo $external_url ?>" id=""></p>
+					<input class="button close-fancy" type="button" value="OK" />
+				</div>
+			</div><!-- .overlay -->
 
-		
-		<input type="submit" class="button" value="Save"/>
+			
+			<input type="submit" class="button" value="Save"/>
 
-	</div><!-- .buttons -->
+		</div><!-- .buttons -->
 	</form>
-
-	</div><!-- .post content -->
-
-	<div class=" module">
-		    <div id="preview"> </div>
-	</div><!-- .name -->
 </div><!-- .wrap -->
 
 
